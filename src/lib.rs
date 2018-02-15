c31cc729cdf905b5af13a82e244bd8df0681fdde
tummychow <tummychow@users.noreply.github.com>
2018-02-15 11:36:42 -0800

    add git2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+extern crate git2;
+
 use std::error::Error;
 
 pub fn run() -> Result<(), Box<Error>> {
+    let repo = git2::Repository::open_from_env()?;
+    println!("{:?}", repo.path());
+
     Ok(())
 }
 

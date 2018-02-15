17ec779cc5b2c5931442623f611b61c7334a308a
tummychow <tummychow@users.noreply.github.com>
2018-02-15 14:39:34 -0800

    walk over target revision stack

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,33 @@ pub fn run(config: &Config) -> Result<(), Box<Error>> {
     let repo = git2::Repository::open_from_env()?;
     debug!(config.logger, "repository found"; "path" => repo.path().to_str());
 
+    let head = repo.head()?;
+    debug!(config.logger, "head found"; "head" => head.name());
+
+    let mut revwalk = repo.revwalk()?;
+    revwalk.set_sorting(git2::SORT_TOPOLOGICAL);
+    revwalk.push_head()?;
+
+    for branch in repo.branches(Some(git2::BranchType::Local))? {
+        let (branch, _) = branch?;
+        let branch = branch.get().name();
+
+        match branch {
+            Some(name) if Some(name) != head.name() => {
+                revwalk.hide_ref(name)?;
+                debug!(config.logger, "branch hidden"; "branch" => branch);
+            }
+            _ => {
+                debug!(config.logger, "branch not hidden"; "branch" => branch);
+            }
+        };
+    }
+
+    for rev in revwalk {
+        let rev = rev?;
+        debug!(config.logger, "rev walked"; "rev" => format!("{}", rev));
+    }
+
     Ok(())
 }
 

9db94f35f435389f72a9066b330f239b93b631e9
tummychow <tummychow@users.noreply.github.com>
2018-02-15 15:08:08 -0800

    refactor stack into fn

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@ extern crate git2;
 #[macro_use]
 extern crate slog;
 
-use std::error::Error;
+use std::error;
+use std::fmt;
 
 pub struct Config<'a> {
     pub dry_run: bool,
@@ -10,16 +11,46 @@ pub struct Config<'a> {
     pub logger: &'a slog::Logger,
 }
 
-pub fn run(config: &Config) -> Result<(), Box<Error>> {
+pub fn run(config: &Config) -> Result<(), Box<error::Error>> {
     let repo = git2::Repository::open_from_env()?;
     debug!(config.logger, "repository found"; "path" => repo.path().to_str());
 
+    working_stack(&repo, config.logger)?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+pub struct Error(String);
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        &self.0
+    }
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
+}
+
+fn working_stack(
+    repo: &git2::Repository,
+    logger: &slog::Logger,
+) -> Result<Vec<git2::Oid>, Box<error::Error>> {
     let head = repo.head()?;
-    debug!(config.logger, "head found"; "head" => head.name());
+    debug!(logger, "head found"; "head" => head.name());
+
+    if !head.is_branch() {
+        return Err(Box::new(Error(String::from("HEAD is not a branch"))));
+    }
 
     let mut revwalk = repo.revwalk()?;
     revwalk.set_sorting(git2::SORT_TOPOLOGICAL);
     revwalk.push_head()?;
+    debug!(logger, "head pushed"; "head" => head.name());
 
     for branch in repo.branches(Some(git2::BranchType::Local))? {
         let (branch, _) = branch?;
@@ -28,20 +59,21 @@ pub fn run(config: &Config) -> Result<(), Box<Error>> {
         match branch {
             Some(name) if Some(name) != head.name() => {
                 revwalk.hide_ref(name)?;
-                debug!(config.logger, "branch hidden"; "branch" => branch);
+                debug!(logger, "branch hidden"; "branch" => branch);
             }
             _ => {
-                debug!(config.logger, "branch not hidden"; "branch" => branch);
+                debug!(logger, "branch not hidden"; "branch" => branch);
             }
         };
     }
 
+    let mut ret = Vec::new();
     for rev in revwalk {
         let rev = rev?;
-        debug!(config.logger, "rev walked"; "rev" => format!("{}", rev));
+        ret.push(rev);
+        debug!(logger, "rev walked"; "rev" => format!("{}", rev));
     }
-
-    Ok(())
+    Ok(ret)
 }
 
 #[cfg(test)]

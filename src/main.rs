e7d10d5f0a00535f5d29eead74d043f1d9cd7dfd
tummychow <tummychow@users.noreply.github.com>
2018-02-15 11:47:42 -0800

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+#[macro_use]
+extern crate clap;
 extern crate git_absorb;
 
-use std::env;
 use std::process;
 
 fn main() {
+    let args = app_from_crate!().get_matches();
+    println!("{:?}", args);
+
     if let Err(e) = git_absorb::run() {
         eprintln!("error: {:?}", e);
         process::exit(1);
     }
-
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 }

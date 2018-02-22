e60cb96db6ecf00c93d805f2ceee233f14d84053
tummychow <tummychow@users.noreply.github.com>
2018-02-21 22:50:27 -0800

    implement interleave check
    hunks that overlap can still commute if their contents are completely
    uniform

--- src/commute.rs
+++ src/commute.rs
@@ -36,6 +36,23 @@ fn anchors(hunk: &owned::Hunk) -> (usize, usize, usize, usize) {
     }
 }
 
+/// Tests if all elements of the iterator are equal to each other.
+///
+/// An empty iterator returns `true`.
+///
+/// `uniform()` is short-circuiting. It will stop processing as soon
+/// as it finds two pairwise inequal elements.
+fn uniform<I, E>(mut iter: I) -> bool
+where
+    I: ::std::iter::Iterator<Item = E>,
+    E: ::std::cmp::Eq,
+{
+    match iter.next() {
+        Some(first) => iter.all(|e| e == first),
+        None => true,
+    }
+}
+
 fn commute(
     first: &owned::Hunk,
     second: &owned::Hunk,
@@ -51,6 +68,20 @@ fn commute(
         _ => return Err(failure::err_msg("nonsensical hunk ordering")),
     };
 
+    // if both hunks are exclusively adding or removing, and both
+    // hunks are composed entirely of the same line being repeated,
+    // then they commute no matter what their offsets are, because
+    // they can be interleaved in any order without changing the final
+    // result
+    let interleavable = {
+        if above.added.lines.is_empty() && below.added.lines.is_empty() {
+            uniform(above.removed.lines.iter().chain(&*below.removed.lines))
+        } else if above.removed.lines.is_empty() && below.removed.lines.is_empty() {
+            uniform(above.added.lines.iter().chain(&*below.added.lines))
+        } else {
+            false
+        }
+    };
     // there has to be at least one unchanged line between the two
     // hunks on the first hunk's added side, and the second hunk's
     // removed side
@@ -59,8 +90,9 @@ fn commute(
     } else {
         (anchors(above).1, anchors(below).2)
     };
-    // the hunks overlap, and cannot commute
-    if above_anchor > below_anchor {
+    // the hunks overlap and are not interleavable, so they cannot
+    // commute
+    if above_anchor > below_anchor && !interleavable {
         return Ok(None);
     }
 
@@ -114,5 +146,38 @@ mod tests {
         let (new1, new2) = commute(&hunk1, &hunk2).unwrap().unwrap();
         assert_eq!(new1.added.start, 1);
         assert_eq!(new2.added.start, 3);
+    }
+
+    #[test]
+    fn test_commute_interleave() {
+        let mut line = ::std::iter::repeat(b"bar\n".to_vec());
+        let hunk1 = owned::Hunk {
+            added: owned::Block {
+                start: 1,
+                lines: Rc::new((&mut line).take(4).collect::<Vec<_>>()),
+                trailing_newline: true,
+            },
+            removed: owned::Block {
+                start: 0,
+                lines: Rc::new(vec![]),
+                trailing_newline: true,
+            },
+        };
+        let hunk2 = owned::Hunk {
+            added: owned::Block {
+                start: 1,
+                lines: Rc::new((&mut line).take(2).collect::<Vec<_>>()),
+                trailing_newline: true,
+            },
+            removed: owned::Block {
+                start: 0,
+                lines: Rc::new(vec![]),
+                trailing_newline: true,
+            },
+        };
+
+        let (new1, new2) = commute(&hunk1, &hunk2).unwrap().unwrap();
+        assert_eq!(new1.added.lines.len(), 2);
+        assert_eq!(new2.added.lines.len(), 4);
     }
 }

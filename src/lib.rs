tummychow/git-absorb

commit fb9e1b3950ab9b821ad402098c34bac46cfbd453

Only update head_tree when we commit.
Fixes the case where if we pass `--dry-run` the additions/deletions
keep growing in number since we never "reset" the tree back.

src/lib.rs
@@ -255,7 +255,7 @@ fn run_with_repo(config: &Config, repo: &git2::Repository) -> Result<()> {
         .iter()
         .zip(hunks_with_commit.iter().skip(1).map(Some).chain([None]))
     {
-        head_tree = apply_hunk_to_tree(
+        let new_head_tree = apply_hunk_to_tree(
             &repo,
             &head_tree,
             &current.hunk_to_apply,
@@ -279,9 +279,10 @@ fn run_with_repo(config: &Config, repo: &git2::Repository) -> Result<()> {
                 .filter(|&msg| summary_counts[msg] == 1)
                 .unwrap_or(&dest_commit_id);
             let diff = repo
-                .diff_tree_to_tree(Some(&head_commit.tree()?), Some(&head_tree), None)?
+                .diff_tree_to_tree(Some(&head_commit.tree()?), Some(&new_head_tree), None)?
                 .stats()?;
             if !config.dry_run {
+                head_tree = new_head_tree;
                 head_commit = repo.find_commit(repo.commit(
                     Some("HEAD"),
                     &signature,

commit c35b4dfce60399f649c4bbd0e5650ce6fe4fb923

Update `head_tree` when no commit happens.
This regression was introduced in
fb9e1b3950ab9b821ad402098c34bac46cfbd453.

Moreover, the tests didn't catch it because they didn't check that
there are no leftover changes.

src/lib.rs
@@ -301,6 +301,9 @@ fn run_with_repo(config: &Config, repo: &git2::Repository) -> Result<()> {
                       "header" => format!("+{},-{}", diff.insertions(), diff.deletions()),
                 );
             }
+        } else {
+            // we didn't commit anything, but we applied a hunk
+            head_tree = new_head_tree;
         }
     }
 
@@ -500,6 +503,18 @@ lines
         ctx
     }
 
+    fn nothing_left_in_index(ctx: Context) {
+        let head = ctx.repo.head().unwrap().peel_to_tree().unwrap();
+        let diff = ctx
+            .repo
+            .diff_tree_to_index(Some(&head), Some(&ctx.repo.index().unwrap()), None)
+            .unwrap();
+        let stats = diff.stats().unwrap();
+        assert_eq!(stats.files_changed(), 0);
+        assert_eq!(stats.insertions(), 0);
+        assert_eq!(stats.deletions(), 0);
+    }
+
     #[test]
     fn multiple_fixups_per_commit() {
         let ctx = prepare_and_stage();
@@ -521,6 +536,8 @@ lines
         let mut revwalk = ctx.repo.revwalk().unwrap();
         revwalk.push_head().unwrap();
         assert_eq!(revwalk.count(), 3);
+
+        nothing_left_in_index(ctx);
     }
 
     #[test]
@@ -544,5 +561,7 @@ lines
         let mut revwalk = ctx.repo.revwalk().unwrap();
         revwalk.push_head().unwrap();
         assert_eq!(revwalk.count(), 2);
+
+        nothing_left_in_index(ctx);
     }
 }